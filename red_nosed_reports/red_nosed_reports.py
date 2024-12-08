585ef170a48f8334949d1b5139b596a99da06fb4
Pavan Anand <pavan.anand@pm.me>
2024-12-08 10:13:55 -0800

    feat!: calculate number of safe reports
    Given an input file (e.g., `data/sample.txt`) containing lines of reports of "levels," the program now calculates how many of the reports are "safe," as defined by the puzzle.

--- red_nosed_reports/red_nosed_reports.py
+++ red_nosed_reports/red_nosed_reports.py
@@ -15,4 +15,44 @@ def read_data_file(filename: str) -> str:
         return _file.read()
 
 
-contents = read_data_file("levels.txt")
+def is_ordered(report: tuple[int, ...]) -> bool:
+    return (tuple(sorted(report, reverse=False)) == report
+            or tuple(sorted(report, reverse=True)) == report)
+
+
+def is_unique(report: tuple[int, ...]) -> bool:
+    return len(set(report)) == len(report)
+
+
+def is_appropriately_incrementing(report: tuple[int, ...]) -> bool:
+    _appropriate_increment = True
+    for i, _level in enumerate(report):
+        _prior = report[i-1] if i > 0 else report[i]
+        if _level > _prior + 3 or _level < _prior - 3:
+            _appropriate_increment = False
+            break
+    return _appropriate_increment
+
+
+def determine_safety(report: tuple[int, ...]) -> bool:
+    _ordered: bool = is_ordered(report)
+    _unique: bool = is_unique(report)
+    _incrementing: bool = is_appropriately_incrementing(report)
+    return _ordered and _unique and _incrementing
+
+
+def parse_input(corpus: str) -> tuple[tuple[int, ...], ...]:
+    _contents = corpus.splitlines()
+    return tuple(tuple(map(int, _line.split(" "))) for _line in _contents)
+
+
+def main():
+    contents = read_data_file("level_reports.txt")
+    parsed_contents = parse_input(contents)
+    safe_reports = [determine_safety(_report) for _report in parsed_contents]
+    print("\nPart 1\n------")
+    print(f"The total number of safe reports is {sum(safe_reports):,}.")
+
+
+if __name__ == "__main__":
+    main()
